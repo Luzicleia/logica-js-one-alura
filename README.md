4397ea3939f0966d32907b19e5f50e3a65a35ff1
Luzicleia <luzgomesfy@gmail.com>
2025-07-26 12:07:38 -0300

    primeiro commit: exercícios do curso de lógica

---
+++ README.md
@@ -0,0 +1,10 @@
+# Curso de Lógica de Programação em JavaScript - ONE + Alura
+
+Este repositório contém os exercícios e projetos feitos durante o curso de lógica de programação com JavaScript oferecido pelo programa Oracle Next Education (ONE) em parceria com a Alura.
+
+## Tópicos estudados
+- Variáveis
+- Condicionais
+- Laços
+- Funções
+- Arrays e objetos
---
+++ app.js
@@ -0,0 +1,11 @@
+alert('Boas vindas ao nosso site!');
+let nome = Lua;
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert ("Erro! Preencha todos os campos");
+// condição if  usado para atribuição (=) , para comparação e (==) ou (===), o operador (==) verificar se os valores são iguais, enquanto (===) verificar se os valores e os tipos são iguais.
+if (numeroSecreto == chute) {
+    console.log('Parabéns, você acertou!');
+ }
